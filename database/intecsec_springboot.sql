e0f057355558aa78318a48815475c9de24148fda
Peter.Peng <peter.peng@watsons.com.cn>
2020-06-28 15:54:26 +0800

    移除一个无用的模块

--- database/intecsec_springboot.sql
+++ database/intecsec_springboot.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50617
 File Encoding         : 65001
 
-Date: 2020-06-24 15:33:51
+Date: 2020-06-28 15:52:26
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -34,44 +34,6 @@ CREATE TABLE `blog` (
 -- ----------------------------
 INSERT INTO `blog` VALUES ('1', 'one', 'content', '0', '2020-01-31 11:05:18', '2020-01-31 11:05:27');
 INSERT INTO `blog` VALUES ('2', 'tow', 'tow content', '0', '2020-01-31 11:05:22', '2020-01-31 11:06:10');
-INSERT INTO `blog` VALUES ('3', 'one', 'content', '0', '2018-08-23 17:38:23', '2018-08-23 17:38:23');
-INSERT INTO `blog` VALUES ('4', 'one', 'content', '0', '2018-08-23 17:42:46', '2018-08-23 17:42:46');
-INSERT INTO `blog` VALUES ('5', 'one', 'content', '0', '2018-08-23 17:42:46', '2018-08-23 17:42:46');
-INSERT INTO `blog` VALUES ('6', 'one', 'content', '0', '2018-08-23 17:42:46', '2018-08-23 17:42:46');
-INSERT INTO `blog` VALUES ('7', 'one', 'content', '0', '2018-08-23 17:42:46', '2018-08-23 17:42:46');
-INSERT INTO `blog` VALUES ('8', 'one', 'content', '0', '2018-08-23 17:42:46', '2018-08-23 17:42:46');
-INSERT INTO `blog` VALUES ('9', 'one', 'content', '0', '2018-08-23 17:42:46', '2018-08-23 17:42:46');
-INSERT INTO `blog` VALUES ('10', 'one', 'content', '0', '2018-08-23 17:42:46', '2018-08-23 17:42:46');
-INSERT INTO `blog` VALUES ('11', 'one', 'content', '0', '2018-08-23 17:42:47', '2018-08-23 17:42:47');
-INSERT INTO `blog` VALUES ('12', 'one', 'content', '0', '2018-08-23 17:42:47', '2018-08-23 17:42:47');
-INSERT INTO `blog` VALUES ('13', 'one', 'content', '0', '2018-08-23 17:42:47', '2018-08-23 17:42:47');
-INSERT INTO `blog` VALUES ('14', 'one', 'content', '0', '2018-08-23 17:42:47', '2018-08-23 17:42:47');
-INSERT INTO `blog` VALUES ('15', 'one', 'content', '0', '2018-08-23 17:42:47', '2018-08-23 17:42:47');
-INSERT INTO `blog` VALUES ('16', 'one', 'content', '0', '2018-08-23 17:42:47', '2018-08-23 17:42:47');
-INSERT INTO `blog` VALUES ('17', 'one', 'content', '0', '2018-08-23 17:42:47', '2018-08-23 17:42:47');
-INSERT INTO `blog` VALUES ('18', 'one', 'content', '0', '2018-08-23 17:42:48', '2018-08-23 17:42:48');
-INSERT INTO `blog` VALUES ('19', 'one', 'content', '0', '2018-08-23 17:42:48', '2018-08-23 17:42:48');
-INSERT INTO `blog` VALUES ('20', 'one', 'content', '0', '2018-08-23 17:42:48', '2018-08-23 17:42:48');
-INSERT INTO `blog` VALUES ('21', 'one', 'content', '0', '2018-08-23 17:42:48', '2018-08-23 17:42:48');
-
--- ----------------------------
--- Table structure for id_gen
--- ----------------------------
-DROP TABLE IF EXISTS `id_gen`;
-CREATE TABLE `id_gen` (
-  `id_name` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'ID名称，比如public_id；外部根据此名称来获取id',
-  `next_start_id` bigint(20) NOT NULL DEFAULT '1' COMMENT '当取ID时，获取到的起始ID',
-  `fetch_len` int(11) NOT NULL DEFAULT '100' COMMENT '每次取ID的长度，比如100，返回100个ID',
-  `id_desc` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '备注说明',
-  PRIMARY KEY (`id_name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=COMPACT COMMENT='ID生成表，所有数字ID由ID服务统一生成；注意：该表不允许并发操作，请使用统一的工具（加锁）来操作';
-
--- ----------------------------
--- Records of id_gen
--- ----------------------------
-INSERT INTO `id_gen` VALUES ('item_id', '1000', '1', 'ITEM_ID');
-INSERT INTO `id_gen` VALUES ('order_id', '1000', '1', 'ORDER ID');
-INSERT INTO `id_gen` VALUES ('user_id', '1020', '1', 'USER_ID');
 
 -- ----------------------------
 -- Table structure for mall_item
@@ -114,15 +76,13 @@ CREATE TABLE `mall_order` (
   `gmt_update` datetime DEFAULT CURRENT_TIMESTAMP,
   `delete_mark` tinyint(4) DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Records of mall_order
 -- ----------------------------
-INSERT INTO `mall_order` VALUES ('2', '20200427121526770', '1', '10', '6900', '0', '0', '0', '0', '0', '2020-04-27 12:15:26', '2020-04-27 12:15:26', '0');
-INSERT INTO `mall_order` VALUES ('3', '20200427163123748', '1', '10', '6900', '0', '0', '0', '0', '0', '2020-04-27 16:31:23', '2020-04-27 16:31:23', '0');
-INSERT INTO `mall_order` VALUES ('4', '20200527145347367', '1', '10', '6900', '0', '0', '0', '0', '0', '2020-05-27 14:53:47', '2020-05-27 14:53:47', '0');
-INSERT INTO `mall_order` VALUES ('5', '20200529163726484', '1', '10', '6900', '0', '0', '0', '0', '0', '2020-05-29 16:37:26', '2020-05-29 16:37:26', '0');
+INSERT INTO `mall_order` VALUES ('14', '20200628154433653', '1', '10', '6900', '0', '0', '0', '0', '6900', '2020-06-28 15:44:33', '2020-06-28 15:44:33', '0');
+INSERT INTO `mall_order` VALUES ('15', '20200628154733470', '1', '10', '6900', '0', '0', '0', '0', '6900', '2020-06-28 15:47:33', '2020-06-28 15:47:33', '0');
 
 -- ----------------------------
 -- Table structure for mall_order_consignee
@@ -142,15 +102,13 @@ CREATE TABLE `mall_order_consignee` (
   `gmt_update` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `delete_mark` tinyint(4) DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Records of mall_order_consignee
 -- ----------------------------
-INSERT INTO `mall_order_consignee` VALUES ('2', '1', '1', '广东省', '广州市', '越秀区', '东风东路761号', '陈先生', '15873905393', '2020-04-26 18:47:45', '2020-04-26 18:49:25', '0');
-INSERT INTO `mall_order_consignee` VALUES ('3', '1', '1', '广东省', '广州市', '越秀区', '东风东路761号', '陈先生', '15873905393', '2020-04-26 18:47:45', '2020-04-26 18:49:25', '0');
-INSERT INTO `mall_order_consignee` VALUES ('4', '1', '1', '广东省', '广州市', '越秀区', '东风东路761号', '陈先生', '15873905393', '2020-04-26 18:47:45', '2020-04-26 18:49:25', '0');
-INSERT INTO `mall_order_consignee` VALUES ('5', '1', '1', '广东省', '广州市', '越秀区', '东风东路761号', '陈先生', '15873905393', '2020-04-26 18:47:45', '2020-04-26 18:49:25', '0');
+INSERT INTO `mall_order_consignee` VALUES ('14', '14', '1', '广东省', '广州市', '越秀区', '东风东路761号', '陈先生', '15873905393', '2020-04-26 18:47:45', '2020-04-26 18:49:25', '0');
+INSERT INTO `mall_order_consignee` VALUES ('15', '15', '1', '广东省', '广州市', '越秀区', '东风东路761号', '陈先生', '15873905393', '2020-04-26 18:47:45', '2020-04-26 18:49:25', '0');
 
 -- ----------------------------
 -- Table structure for mall_order_item
@@ -171,19 +129,15 @@ CREATE TABLE `mall_order_item` (
   `gmt_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `delete_mark` tinyint(4) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Records of mall_order_item
 -- ----------------------------
-INSERT INTO `mall_order_item` VALUES ('1', '2', '1', '2600', '2', '沐浴露', '0', '0', '0', '0', '2020-04-27 12:15:26', '2020-06-24 14:31:05', '0');
-INSERT INTO `mall_order_item` VALUES ('2', '2', '2', '4300', '3', '洗发水', '0', '0', '0', '0', '2020-04-27 12:15:26', '2020-06-24 14:31:06', '0');
-INSERT INTO `mall_order_item` VALUES ('3', '3', '1', '2600', '2', '沐浴露', '0', '0', '0', '0', '2020-04-27 16:31:23', '2020-06-24 14:40:21', '0');
-INSERT INTO `mall_order_item` VALUES ('4', '3', '2', '4300', '3', '洗发水', '0', '0', '0', '0', '2020-04-27 16:31:23', '2020-06-24 14:40:23', '0');
-INSERT INTO `mall_order_item` VALUES ('5', '4', '1', '2600', '2', '沐浴露', '0', '0', '0', '0', '2020-05-27 14:53:47', '2020-06-24 14:40:24', '0');
-INSERT INTO `mall_order_item` VALUES ('6', '4', '2', '4300', '3', '洗发水', '0', '0', '0', '0', '2020-05-27 14:53:47', '2020-06-24 14:40:25', '0');
-INSERT INTO `mall_order_item` VALUES ('7', '5', '1', '2600', '2', '沐浴露', '0', '0', '0', '0', '2020-05-29 16:37:26', '2020-06-24 14:40:26', '0');
-INSERT INTO `mall_order_item` VALUES ('8', '5', '2', '4300', '3', '洗发水', '0', '0', '0', '0', '2020-05-29 16:37:26', '2020-06-24 14:40:28', '0');
+INSERT INTO `mall_order_item` VALUES ('22', '14', '1', '2600', '1', '沐浴露', '0', '0', '0', '0', '2020-06-28 15:44:33', '2020-06-28 15:44:33', '0');
+INSERT INTO `mall_order_item` VALUES ('23', '14', '2', '4300', '2', '洗发水', '0', '0', '0', '0', '2020-06-28 15:44:33', '2020-06-28 15:44:33', '0');
+INSERT INTO `mall_order_item` VALUES ('24', '15', '1', '2600', '1', '沐浴露', '0', '0', '0', '0', '2020-06-28 15:47:33', '2020-06-28 15:47:33', '0');
+INSERT INTO `mall_order_item` VALUES ('25', '15', '2', '4300', '2', '洗发水', '0', '0', '0', '0', '2020-06-28 15:47:33', '2020-06-28 15:47:33', '0');
 
 -- ----------------------------
 -- Table structure for mall_user
@@ -229,4 +183,4 @@ CREATE TABLE `mall_user_consignee` (
 -- ----------------------------
 -- Records of mall_user_consignee
 -- ----------------------------
-INSERT INTO `mall_user_consignee` VALUES ('1', '1', '广东省', '广州市', '越秀区', '东风东路761号', '陈先生', '15873905393', '2020-04-26 18:47:45', '2020-04-26 18:49:25', '0');
+INSERT INTO `mall_user_consignee` VALUES ('1', '1', '广东省', '广州市', '天河区', '天河北路11号', '陈先生', '15873905393', '2020-04-26 18:47:45', '2020-06-28 15:49:45', '0');
