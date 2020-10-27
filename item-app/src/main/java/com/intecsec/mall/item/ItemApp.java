e8012f1793c9cd75ef9eef3ce646631354f06335
Peter.Peng <peter.peng@watsons.com.cn>
2020-10-27 18:59:20 +0800

    接入自己rpc 改版后台的 xxl-job

--- item-app/src/main/java/com/intecsec/mall/item/ItemApp.java
+++ item-app/src/main/java/com/intecsec/mall/item/ItemApp.java
@@ -4,6 +4,7 @@ import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ImportResource;
 
 /**
  * @description:
@@ -11,7 +12,9 @@ import org.springframework.context.ApplicationContext;
  * @create: 2020-02-12 21:02
  **/
 @SpringBootApplication
-@EnableDubbo
+// @EnableDubbo
+// @Import(value = { XxlJobConfig.class })
+@ImportResource({ "spring-context.xml" })
 public class ItemApp {
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(ItemApp.class);
---
+++ item-app/src/main/java/com/intecsec/mall/item/jobs/XxlJobSample.java
@@ -0,0 +1,28 @@
+package com.intecsec.mall.item.jobs;
+
+import com.xxl.job.core.biz.model.ReturnT;
+import com.xxl.job.core.handler.annotation.XxlJob;
+import com.xxl.job.core.log.XxlJobLogger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+/**
+ * @author Peter.Peng
+ * @date 2020/10/14
+ */
+@Component
+public class XxlJobSample {
+
+	private static Logger logger = LoggerFactory.getLogger(XxlJobSample.class);
+
+
+	/**
+	 * 1、简单任务示例（Bean模式）
+	 */
+	@XxlJob(value = "demoJobHandler3", cron = "0 0/6 * ? * *", author = "leifeng")
+	public ReturnT<String> demoJobHandler3(String param) throws Exception {
+		XxlJobLogger.log("XXL-JOB, Hello World.");
+		logger.info("XXL-JOB, Hello World.");
+		return ReturnT.SUCCESS;
+	}
+}
